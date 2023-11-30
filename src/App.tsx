4c0c089f3fb05809eabe46b28a79bb9a77ccbe4a
KyleRobison15 <kylerobison15@gmail.com>
2023-11-30 12:34:55 -0700

    Install ChakraUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>Hello Personal Site</h1>
+      <Button colorScheme="blue">Test</Button>
     </>
   );
 }

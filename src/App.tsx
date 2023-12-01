a38c5c9c6b9b95241addf619df49fa439c845019
KyleRobison15 <kylerobison15@gmail.com>
2023-12-01 12:58:17 -0700

    Create color mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { Button } from "@chakra-ui/react";
 import "./App.css";
+import ColorModeSwitch from "./components/ColorModeSwitch";
 
 function App() {
   return (
     <>
-      <h1>Hello Personal Site</h1>
-      <Button colorScheme="blue">Test</Button>
+      <ColorModeSwitch />
     </>
   );
 }

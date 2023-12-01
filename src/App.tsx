1301bffcc5230491d4b3ee628c870e316095256c
KyleRobison15 <kylerobison15@gmail.com>
2023-12-01 16:28:08 -0700

    Make NavBar responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Grid, GridItem, Hide, Show } from "@chakra-ui/react";
 import "./App.css";
 import NavBarMobile from "./components/NavBarMobile";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <Grid gridTemplateAreas={`"nav"`} gridTemplateColumns={`"1fr"`}>
       <GridItem area="nav">
-        <NavBarMobile />
+        <Show below="md">
+          <NavBarMobile />
+        </Show>
+        <Hide below="md">
+          <NavBar />
+        </Hide>
       </GridItem>
     </Grid>
   );

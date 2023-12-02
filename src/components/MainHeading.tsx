17051d08df9ed024f0c4238f468211fd4d52a90b
KyleRobison15 <kylerobison15@gmail.com>
2023-12-01 18:59:48 -0700

    Refactor main heading

---
+++ src/components/MainHeading.tsx
@@ -0,0 +1,26 @@
+import { Flex, HStack, Heading, Image, Text } from "@chakra-ui/react";
+import { headingSizes, textSizes } from "../services/typography-breakpoints";
+import waveEmoji from "../assets/wave.png";
+
+const MainHeading = () => {
+  return (
+    <Flex flexDirection="column" alignItems="end">
+      <HStack mb={3}>
+        <Image
+          src={waveEmoji}
+          aria-label="Waving hand emoji"
+          boxSize="50px"
+          mb={2}
+          mr={2}
+        />
+        <Text fontSize={textSizes}>Hi there! I am,</Text>
+      </HStack>
+      <Heading size={headingSizes}>Kyle Robison</Heading>
+      <Text mt={4} fontSize={textSizes}>
+        Full Stack Developer
+      </Text>
+    </Flex>
+  );
+};
+
+export default MainHeading;
--- src/services/typography-breakpoints.ts
+++ src/services/typography-breakpoints.ts
@@ -1,2 +1,2 @@
 export const headingSizes = ["xl", "2xl", "4xl"];
-export const textSizes = ["xl", "3xl", "5xl"];
+export const textSizes = ["xl", "2xl", "4xl"];
